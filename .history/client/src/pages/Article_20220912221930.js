d9b09ef47f29bc35c1d236e7842c6dd0b0fda750
Sophie <wenxi1988@hotmail.com>
2022-09-12 22:36:17 -0400

    wrap up

--- .history/client/src/pages/Article_20220912221930.js
+++ .history/client/src/pages/Article_20220912221930.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { useParams } from 'react-router-dom'
 import articleContent from './articleContent'
 import Articles from '../components/Articles'
-import NotFound from './NotFound'
 
 
 
